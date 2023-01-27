6d20ef72d941e7993f0da37564dd5fd634d5cdba
SalehMAljohi <salehaljoh33@gmail.com>
2023-01-27 23:16:49 +0300

    change url

--- WebSiteTemplete/App_Start/RouteConfig.cs
+++ WebSiteTemplete/App_Start/RouteConfig.cs
@@ -12,11 +12,16 @@ namespace WebSiteTemplete
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           // routes.MapRoute(
+           //    name: "Default",
+           //    url: "{controller}/{action}/{id}",
+           //    defaults: new { controller = "Home", action = "Main", id = UrlParameter.Optional }
 
+           //);
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Main", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
 
             );
         }
